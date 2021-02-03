8b86052f9dd3e77a7b8a1f32817bd8f0fdfff92c
abah19vo <70108548+abah19vo@users.noreply.github.com>
2021-02-03 15:07:54 +0100

    TGOM docker setup

---
+++ db/init.sql
@@ -0,0 +1,4 @@
+CREATE TABLE humans(
+   name VARCHAR(50) ,
+    age INT(11) 
+)
---
+++ website/src/presentaion-layer/app.js
@@ -0,0 +1,61 @@
+const express = require("express")
+const expressHandlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
+const mysql = require("mysql2")
+const path = require("path")
+
+const app = express()
+
+
+
+
+
+app.engine('hbs', expressHandlebars({
+    defaultLayout: 'main.hbs',
+    extname: "hbs"
+}))
+
+const connection = mysql.createConnection({
+    host: 'db',
+    user: 'root',
+    database: 'hello',
+    password: "abc123"
+});
+
+
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+
+app.set('views',path.join(__dirname,'views'))
+app.use(express.static(path.join(__dirname,'public')))
+
+/*app.use(express.static('views/CSS'))
+app.use(express.static('views/images'))
+*/
+app.get("/",function(request,response){
+    response.send("Hello Wod")
+    console.log("Bye")
+
+    connection.query(
+        'SELECT * FROM `humans`',
+        function(err, results, fields) {
+          console.log(results); // results contains rows returned by server
+          console.log(fields); // fields contains extra meta data about results, if available
+        }
+    )
+})
+
+app.get('/contact', (req, res) => {
+    res.render('contact.hbs')
+})
+
+
+app.listen(8080)
+
+
+   
+  
+   
+  
