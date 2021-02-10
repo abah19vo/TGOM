07d75b03cb2b2a05e0d6188ab9956ff525334aa5
Ivin benjamin <55826241+raswer4@users.noreply.github.com>
2021-02-10 17:41:56 +0100

    just commit ffs

---
+++ website/src/data-access-layer/user_table.js
@@ -0,0 +1,37 @@
+const mysql = require("mysql2")
+
+
+
+const connection = mysql.createConnection({
+    host: 'db',
+    user: 'root',
+    database: 'hello',
+    password: "abc123"
+})
+
+exports.createUser = function(username,password,name,callback){
+    const query = " INSERT INTO user(username,password,name) VALUES(?,?,?)"
+    const values = [username,password,name]
+    connection.query(query, values, function(error){
+        callback(error)
+    })
+}
+
+exports.getUserById = function(id,callback){
+    const query = " SELECT * FROM user WHERE id = ?"
+    const values = [id]
+    connection.query(query,values,function(error,user){
+        callback(error,user)
+    })
+}
+
+exports.getAllUsers = function(id,callback){
+    const query = " SELECT * FROM user WHERE id = ?"
+    connection.query(query,function(error,user){
+        callback(error,user)
+    })
+}
+
+
+
+/*'SELECT * FROM `user`'*/
