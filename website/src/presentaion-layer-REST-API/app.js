a007c30b67f77be6f646ab950e4a563e7bf914d8
abah19vo <70108548+abah19vo@users.noreply.github.com>
2021-03-23 14:04:51 +0100

    klmkl

--- website/src/presentaion-layer-REST-API/app.js
+++ website/src/presentaion-layer-REST-API/app.js
@@ -15,10 +15,12 @@ module.exports = function({userRouterAPI,feedbackRouterAPI}){
   }))
 
   app.use(function(req, res, next){
+    
     res.setHeader('Access-Control-Allow-Origin',"*")
     res.setHeader('Access-Control-Allow-Methods',"*")
     res.setHeader('Access-Control-Allow-Headers',"*")
     res.setHeader('Access-Control-Allow-Expose-Headers',"*")
+
     next()
   })
 
